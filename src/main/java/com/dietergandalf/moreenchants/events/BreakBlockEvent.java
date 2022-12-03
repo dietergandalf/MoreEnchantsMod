be8f7012a39e953ca284cf3d7efde874ea86a970
Malte <maltebothe@web.de>
2022-12-03 20:17:57 +0100

    Timber Enchantment better features

--- src/main/java/com/dietergandalf/moreenchants/events/BreakBlockEvent.java
+++ src/main/java/com/dietergandalf/moreenchants/events/BreakBlockEvent.java
@@ -3,23 +3,19 @@ package com.dietergandalf.moreenchants.events;
 import com.dietergandalf.moreenchants.init.ModEnchantments;
 import com.dietergandalf.moreenchants.util.util;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.enchantment.Enchantments;
-import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 
 import java.util.ArrayList;
 
+import static com.dietergandalf.moreenchants.functions.BreakBlockHelper.destroyBlockDropAtPlayer;
+import static com.dietergandalf.moreenchants.functions.BreakBlockHelper.getAdjacentTreeBlocks;
 import static com.dietergandalf.moreenchants.util.util.*;
 
 
@@ -65,8 +61,9 @@ public class BreakBlockEvent implements PlayerBlockBreakEvents.After {
                     destroyBlockDropAtPlayer(world, blockPos, tmp_state, player, true);
                 }
             }
+        }
 
-        } else if (timber>0 && player.getMainHandStack().getItem().isSuitableFor(state)) {
+        if (timber>0 && player.getMainHandStack().getItem().isSuitableFor(state)) {
             ArrayList<BlockPos> AdjacentBlocks = new ArrayList<>();
             ArrayList<util.Tuple> sapling_list = new ArrayList<>();
             getAdjacentTreeBlocks(AdjacentBlocks, sapling_list, world, pos, 0, 6000);
@@ -82,84 +79,5 @@ public class BreakBlockEvent implements PlayerBlockBreakEvents.After {
 
     }
 
-    /*
-     * Destroy the block and drop the block's drops
-
-    private static void destroyBlock(World world, BlockPos pos, BlockState state, PlayerEntity player) {
-        int fortuneLvl = EnchantmentHelper.getEquipmentLevel(Enchantments.FORTUNE, player);
-        int silktouch = EnchantmentHelper.getEquipmentLevel(Enchantments.SILK_TOUCH, player);
-        world.breakBlock(pos, false, player, 11);
-        world.setBlockState(pos, Blocks.AIR.getDefaultState(), 11);
-
-        if(silktouch == 1){
-            ItemStack itemStack = new ItemStack(state.getBlock(), 1);
-            ItemEntity blockDrop = new ItemEntity(world, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, itemStack);
-            world.spawnEntity(blockDrop);
-        }else{
-            Block.getDroppedStacks(state, (ServerWorld) world, pos, null).forEach(itemStack -> {
-
-                if(!itemStack.getItem().equals(state.getBlock().asItem())) {
-                    itemStack.setCount(fortuneLvl + 1);
-                }
-                ItemEntity blockDrop = new ItemEntity(world, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, itemStack);
-                world.spawnEntity(blockDrop);
-
-            });
-        }
-    }
-    */
-
-    /*
-     * Destroy the block and drop the block's drops at the player
-     */
-    private static void destroyBlockDropAtPlayer(World world, BlockPos pos, BlockState state, PlayerEntity player, boolean standard) {
-        int fortuneLvl = EnchantmentHelper.getEquipmentLevel(Enchantments.FORTUNE, player);
-        int silktouch = EnchantmentHelper.getEquipmentLevel(Enchantments.SILK_TOUCH, player);
-
-        if(world.getBlockState(pos).getBlock().toString().contains("shulker"))return;
-        world.breakBlock(pos, false, player, 11);
-        world.setBlockState(pos, Blocks.AIR.getDefaultState(), 11);
-
-        if(silktouch == 1){
-            ItemStack itemStack = new ItemStack(state.getBlock(), 1);
-            ItemEntity blockDrop = new ItemEntity(world, player.getBlockX()+ 0.5, player.getBlockY() + 0.5, player.getBlockZ() + 0.5, itemStack);
-            world.spawnEntity(blockDrop);
-        }else{
-            Block.getDroppedStacks(state, (ServerWorld) world, pos, null).forEach(itemStack -> {
-
-                if(!standard && !itemStack.getItem().equals(state.getBlock().asItem())) {
-                    itemStack.setCount(fortuneLvl + 1);
-                }else if(itemStack.getItem().equals(state.getBlock().asItem())){
-                    itemStack.setCount(fortuneLvl+1);
-                }
-                ItemEntity blockDrop = new ItemEntity(world, player.getBlockX()+ 0.5, player.getBlockY() + 0.5, player.getBlockZ() + 0.5, itemStack);
-                world.spawnEntity(blockDrop);
-
-            });
-        }
-    }
-
-    public static void getAdjacentTreeBlocks(ArrayList<BlockPos> list, ArrayList<util.Tuple> sapling_list, World world, BlockPos pos, int curDepth, int maxDepth){
-        if(curDepth>maxDepth)return;
-        list.add(pos);
-        BlockState state = world.getBlockState(pos);
-        if(checkDirt(world.getBlockState(new BlockPos(pos.getX(), pos.getY()-1, pos.getZ())).getBlock()) && checkLog(state.getBlock())){
-            String name = world.getBlockState(pos).getBlock().getName().toString();
-            BlockState sapling = getSapling(name);
-            util.Tuple t = new util.Tuple(pos, sapling);
-            sapling_list.add(t);
-
-        }
-        for (int i = -1; i <= 1; i++) {
-            for (int k = -1; k <= 1; k++) {
-                for (int j = -1; j <= 1; j++) {
-                    BlockPos tmpPos = new BlockPos(pos.getX() + i, pos.getY() + j, pos.getZ() + k);
-                    BlockState blockState = world.getBlockState(tmpPos);
-                    if (checkTree(blockState.getBlock()) && !list.contains(tmpPos)) {
-                        getAdjacentTreeBlocks(list, sapling_list, world, tmpPos, curDepth+1, maxDepth);
-                    }
-                }
-            }
-        }
-    }
 }
+
--- src/main/java/com/dietergandalf/moreenchants/util/util.java
+++ src/main/java/com/dietergandalf/moreenchants/util/util.java
@@ -17,14 +17,8 @@ public class util {
             Blocks.ACACIA_LOG, Blocks.ACACIA_LEAVES,
             Blocks.JUNGLE_LOG, Blocks.JUNGLE_LEAVES,
             Blocks.CRIMSON_STEM, Blocks.NETHER_WART_BLOCK,
-            Blocks.WARPED_STEM, Blocks.WARPED_WART_BLOCK
-            );
-
-    public static final List<Block> TREE_STEM = List.of(
-            Blocks.OAK_LOG, Blocks.SPRUCE_LOG,
-            Blocks.DARK_OAK_LOG, Blocks.BIRCH_LOG,
-            Blocks.ACACIA_LOG, Blocks.JUNGLE_LOG,
-            Blocks.CRIMSON_STEM, Blocks.WARPED_STEM
+            Blocks.WARPED_STEM, Blocks.WARPED_WART_BLOCK,
+            Blocks.SHROOMLIGHT
             );
 
     public static final List<BlockState> SAPLINGS = List.of(Blocks.OAK_SAPLING.getDefaultState(),
@@ -33,16 +27,18 @@ public class util {
             Blocks.BIRCH_SAPLING.getDefaultState(),
             Blocks.CRIMSON_FUNGUS.getDefaultState(), Blocks.WARPED_FUNGUS.getDefaultState());
 
+    public static final List<Block> LEAVES = List.of(Blocks.NETHER_WART_BLOCK, Blocks.WARPED_WART_BLOCK,
+        Blocks.OAK_LEAVES, Blocks.SPRUCE_LEAVES, Blocks.DARK_OAK_LEAVES, Blocks.BIRCH_LEAVES, Blocks.ACACIA_LEAVES, Blocks.BIRCH_LEAVES, Blocks.JUNGLE_LEAVES);
 
     public static final List<Block> DIRT_VARIANT = List.of(Blocks.DIRT, Blocks.GRASS, Blocks.PODZOL);
     public static final List<Block> NETHERRACK_VARIANTS = List.of(Blocks.NETHERRACK, Blocks.WARPED_NYLIUM, Blocks.CRIMSON_NYLIUM);
 
+    public static boolean checkLeave(Block block) { return LEAVES.contains(block); }
     public static boolean checkTree(Block block){
         return TREE_BLOCKS.contains(block);
     }
-    public static boolean checkLog(Block block) {return TREE_STEM.contains(block);}
-    public static boolean checkDirt(Block block){return DIRT_VARIANT.contains(block);}
-    public static boolean checkNetherrack(Block block){return NETHERRACK_VARIANTS.contains(block);}
+    public static boolean checkDirt(Block block) { return DIRT_VARIANT.contains(block); }
+    public static boolean checkNetherrack(Block block) { return NETHERRACK_VARIANTS.contains(block); }
 
     public static BlockState getSapling(String name){
         String s = name.substring(33, name.length()-15);
