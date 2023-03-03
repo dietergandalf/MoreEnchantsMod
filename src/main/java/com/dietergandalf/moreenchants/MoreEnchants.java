fc8dba8030fa314609a02dbae82e6919f324117f
Malte <maltebothe@web.de>
2023-03-03 20:32:05 +0100

    1.19.3, Ore Spawning rare

--- src/main/java/com/dietergandalf/moreenchants/MoreEnchants.java
+++ src/main/java/com/dietergandalf/moreenchants/MoreEnchants.java
@@ -6,8 +6,15 @@ import com.dietergandalf.moreenchants.init.ModBlocks;
 import com.dietergandalf.moreenchants.init.ModEnchantments;
 import com.dietergandalf.moreenchants.init.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.PlacedFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +23,7 @@ public class MoreEnchants implements ModInitializer {
     public static final String MOD_ID = "enchants";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    public static final RegistryKey<PlacedFeature> RUBY_ORE_PLACED_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(MOD_ID, "ruby_gen"));
 
     @Override
     public void onInitialize() {
@@ -24,6 +32,7 @@ public class MoreEnchants implements ModInitializer {
         ModBlocks.RegisterBlocks();
         ModEnchantments.RegisterEnchantments();
 
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, RUBY_ORE_PLACED_KEY);
 
         ServerTickEvents.START_SERVER_TICK.register(new TickHandler());
         PlayerBlockBreakEvents.AFTER.register(new BreakBlockEvent());
